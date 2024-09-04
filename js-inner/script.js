3ff78fe598c718eb119b66b1fd3050cd68925256
cmxrenato <renatoaraujoleal@gmail.com>
2024-09-04 18:29:31 -0300

    FInalizado

---
+++ js-inner/script.js
@@ -0,0 +1,18 @@
+let elementoH1 = document.querySelector('#titulo');
+elementoH1.innerText='Olá mundo!';
+
+let anchor = document.querySelector('body a');
+anchor.innerText='Clique aqui para ir ao site da Proz';
+
+let ul = document.querySelector('body ul');
+ul.innerHTML = `
+<li>Item 1</li>
+<li>Item 2</li>
+<li>Item 3</li>`;
+
+let ol = document.querySelector('#lista-ordenada');
+ol.innerHTML =` 
+        <a href=""><li>Item 1 link</li></a>
+        <a href=""><li>Item 2 link</li></a>
+        <a href=""><li>Item 3 link</li></a>`;
+        
