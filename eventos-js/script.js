dc22e5f5fd3d1c029253c14a695505cfd6ee94a4
cmxrenato <renatoaraujoleal@gmail.com>
2024-09-11 20:54:17 -0300

    novos eventos

--- eventos-js/script.js
+++ eventos-js/script.js
@@ -1,3 +1,4 @@
+/*
 let numero = document.querySelector('h1');
 let botao = document.querySelectorAll('button');
 let contador = 0;
@@ -11,4 +12,25 @@ function subtrairUm(){
     numero.innerText = contador;
 }
 botao[0].addEventListener("click", adicionarUm); //Essa função precisa de dois parâmetros. 
-botao[1].addEventListener("click", subtrairUm);
+botao[1].addEventListener("click", subtrairUm); */
+
+let button = document.querySelector('button');
+let span = document.querySelector('span');
+let section = document.querySelector('section');
+
+button.addEventListener("mouseover", ()=>{
+    span.style.opacity = "100";
+});
+
+button.addEventListener("mouseout", ()=>{
+    span.style.opacity = "0";
+});
+
+button.addEventListener("click", ()=>{
+    section.innerText = "Fez um clique simples!";
+
+});
+
+button.addEventListener("dblclick", ()=>{
+    section.innerText = "Fez um duplo clique";
+})
