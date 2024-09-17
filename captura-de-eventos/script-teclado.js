f856f8e1a7ac13972f01deb3d23a23575a5defea
cmxrenato <renatoaraujoleal@gmail.com>
2024-09-17 19:14:57 -0300

    Alguns ajustes

--- captura-de-eventos/script-teclado.js
+++ captura-de-eventos/script-teclado.js
@@ -2,9 +2,8 @@ let keyText = document.querySelector("#key-text");
 let codeText = document.querySelector("#code-text");
 let quadrado = document.querySelector("#quadrado");
 let distanciaDaEsquerda = 0;
-let distanciaDaDireita = 0;
 let distanciaDeCima = 0;
-let distanciaDeBaixo = 0;
+
 
 
 
@@ -22,23 +21,23 @@ document.addEventListener("keydown", (e)=>{
         distanciaDaEsquerda = distanciaDaEsquerda + 10;
         console.log(distanciaDaEsquerda);
         quadrado.style.left = distanciaDaEsquerda + "px";
-    }
+    };
     if (e.code == "ArrowDown"){
        
         distanciaDeCima = distanciaDeCima + 10;
         console.log(distanciaDeCima);
         quadrado.style.top = distanciaDeCima + "px";
-    }
+    };
     if (e.code == "ArrowUp"){
         
         distanciaDeCima = distanciaDeCima - 10;
         console.log(distanciaDeCima);
         quadrado.style.top = distanciaDeCima + "px";
-    }
+    };
     if (e.code == "ArrowLeft"){
        
         distanciaDaEsquerda = distanciaDaEsquerda - 10;
         console.log(distanciaDaEsquerda);
         quadrado.style.left = distanciaDaEsquerda + "px";
-    }
+    };
 });
